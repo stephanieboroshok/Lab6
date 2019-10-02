de763b970794849656eb7e3ddb85162f4fa717bf
stephanieboroshok <sb8485a@american.edu>
2019-10-02 10:07:45 -0400

    added comments

--- Problem 1.py
+++ Problem 1.py
@@ -6,14 +6,21 @@
 def count_EvensOdds(numbers):
     oddCounter = 0
     evenCounter = 0
+    #create variables with counts inside function
     for i in numbers:
         if i %2==0:
+            #i=individual element of list
+            #modulus gets remainder to determine even or odd
             evenCounter = evenCounter + 1
         else:
             oddCounter = oddCounter + 1
+            #add to counters based on elements being even or odd
     print(evenCounter)
     print(oddCounter)
+    #print even or odd determinations
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#create list for function to examine
 
 print (count_EvensOdds(numbers))
+#print the function
