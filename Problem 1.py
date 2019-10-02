2df1c9af86449aee7943caa8fc293404d92c7eb9
stephanieboroshok <sb8485a@american.edu>
2019-10-02 10:27:00 -0400

    added comments and problem 2

--- Problem 1.py
+++ Problem 1.py
@@ -6,6 +6,8 @@
 def count_EvensOdds(numbers):
     oddCounter = 0
     evenCounter = 0
+    highestValue = 0
+    lowestValue = numbers[0]
     #create variables with counts inside function
     for i in numbers:
         if i %2==0:
@@ -15,9 +17,21 @@ def count_EvensOdds(numbers):
         else:
             oddCounter = oddCounter + 1
             #add to counters based on elements being even or odd
-    print(evenCounter)
-    print(oddCounter)
-    #print even or odd determinations
+        if i > highestValue:
+            highestValue = i
+            #if current number we are on is greater than the stored current value, replace it as the highest value
+        else:
+            pass
+        if i < lowestValue:
+            lowestValue = i
+            #if current number we are on is less than stored current value, replace as lowest value
+        else:
+            pass
+    print('There are', evenCounter, 'even numbers in the list')
+    print('There are', oddCounter, 'odd numbers in the list')
+    print('The highest value in the list is', highestValue)
+    print('The lowest value in the list is', lowestValue)
+    #print even or odd determinations and highest and lowest values
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 #create list for function to examine
